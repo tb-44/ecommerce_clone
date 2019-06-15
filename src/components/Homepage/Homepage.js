3d1acdc9597ea97f3a5ae38e894cac6111193278
Trent Bennett <rehab.rtb@gmail.com>
2019-06-14 19:39:22 -0600

    first commit

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import { Link } from 'react-router-dom';
+//import './homepage.css';
+
+export default function Homepage (props) {
+  return (
+
+    <main className="homepageContainer">
+      <div>this is the homepage</div>
+    </main>
+
+  )
+}
